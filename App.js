bed0a03e50fd2d13324a6ceda156002238bf3df5
Richard <grnganon@gmail.com>
2022-07-03 00:06:42 -0400

    Screen Placeholders!

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StartScreen from "./screens/StartScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <StartScreen />
       <Text>Hello World!!!!</Text>
       <StatusBar style="auto" />
     </View>
---
+++ screens/StartScreen.js
@@ -0,0 +1,13 @@
+import {View, TextInput} from "react-native";
+
+const StartScreen = () => {
+    return (
+        <View>
+        <TextInput>
+            Button Placeholders
+        </TextInput>
+    </View>
+    );
+}
+
+export default StartScreen
